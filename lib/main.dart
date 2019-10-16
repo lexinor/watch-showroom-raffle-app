e1a06e1fe9ae91b6e9b1714e686ef307b7cf4244
lexinor <alternonolimit@gmail.com>
2019-10-16 15:52:46 +0200

    Added Slider to homepage

--- lib/main.dart
+++ lib/main.dart
@@ -1,93 +1,73 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_swiper/flutter_swiper.dart';
 
-void main() => runApp(MyApp());
+void main() => runApp(MaterialApp(
+    home: Home()
 
-class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
+));
+
+class Home extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-
-        primarySwatch: Colors.blue,
-      ),
-      home: MyHomePage(),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key}) : super(key: key);
-
-
-  @override
-  _MyHomePageState createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
       appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.display1,
-            ),
-          ],
+        title: Text("Augarde Showroom", style: TextStyle(
+          fontSize: 25.0 ),
         ),
+        centerTitle: true ,
+        backgroundColor: Colors.deepPurpleAccent,
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
-    );
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.center,
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        children: <Widget>[
+          Expanded(
+            child: new Swiper(
+              itemBuilder: (BuildContext context, int index) {
+                return new Image.network(
+                  "http://via.placeholder.com/288x188",
+                  fit: BoxFit.fill,
+                );
+              },
+              itemCount: 10,
+              viewportFraction: 0.8,
+              scale: 0.9,
+            ),
+          ),
+          Expanded(
+            child: Text("Produits"),
+          ),
+
+          Expanded(
+            child: new Swiper(
+                layout: SwiperLayout.CUSTOM,
+                customLayoutOption: new CustomLayoutOption(
+                    startIndex: -1,
+                    stateCount: 3
+                ).addRotate([
+                  -45.0/180,
+                  0.0,
+                  45.0/180
+                ]).addTranslate([
+                  new Offset(370.0, 0),
+                  new Offset(0.0, 0.0),
+                  new Offset(-370.0, 0)
+                ]),
+                itemWidth: 300.0,
+                itemHeight: 200.0,
+                itemBuilder: (context, index) {
+                  return new Container(
+                    color: Colors.grey,
+                    child: new Center(
+                      child: new Text("$index"),
+                    ),
+                  );
+                },
+                itemCount: 10),
+          )
+        ],
+      ),
+      );
   }
 }
